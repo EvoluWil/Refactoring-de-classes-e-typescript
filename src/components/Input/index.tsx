737b8801f0f1681bc5f9713b5ac4d74889236921
Willian-Rodrigues <willian.rsilva1994@gmail.com>
2021-10-28 07:28:32 -0300

    updateProject to TS and Function components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,23 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
+import { IconType } from "react-icons";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon?: IconType;
+  placeholder?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  name,
+  icon: Icon,
+  ...rest
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -31,7 +38,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
@@ -43,6 +50,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
+        placeholder={placeholder}
         ref={inputRef}
         {...rest}
       />
